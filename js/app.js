4cecff01d2e0779851ab9b88e433125e46049f36
Victor Blanco <victorblanco@ordersmadesimple.com>
2022-01-30 18:41:32 +0000

    sunday commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 import * as Cards from "./cards.js";
 
 console.log(Cards);
+
+const pattern = document.querySelectorAll(".pattern");
+const options = document.querySelectorAll(".options");
+const cardBtn = document.querySelectorAll(".card_btn");
+const counter = document.querySelectorAll(".counter");
