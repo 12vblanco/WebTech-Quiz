1fcdce0c05738b2697cd59e909a94c20bd44e695
Victor Blanco <victorblanco@ordersmadesimple.com>
2022-01-30 15:15:52 +0000

    reduce font size

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 import * as Cards from "./cards.js";
 
-console.log(Cards.beautyCard);
-
-const answer1 = document.querySelector("#answer1");
-answer1.innerText = Cards.beautyCard.choice1;
+const answer1 = document.querySelector(".ans1");
+answer1.innerHTML = Cards.beautyCard.choice1;
+const answer2 = document.querySelector(".ans2");
+answer2.innerHTML = Cards.beautyCard.choice2;
+const answer3 = document.querySelector(".ans3");
+answer3.innerHTML = Cards.beautyCard.choice3;
+const answer4 = document.querySelector(".ans4");
+answer4.innerHTML = Cards.beautyCard.choice4;
