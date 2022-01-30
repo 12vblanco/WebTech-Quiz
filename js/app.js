f741579cdb1c42ecbf41403c5f8d72dc0d07610c
Victor Blanco <victorblanco@ordersmadesimple.com>
2022-01-30 18:04:21 +0000

    1

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
 import * as Cards from "./cards.js";
 
-function oneOfTen() {
-  const answer1 = document.querySelector(".ans1");
-  answer1.innerHTML = Cards.beautyCard.choice1;
-  const answer2 = document.querySelector(".ans2");
-  answer2.innerHTML = Cards.beautyCard.choice2;
-  const answer3 = document.querySelector(".ans3");
-  answer3.innerHTML = Cards.beautyCard.choice3;
-  const answer4 = document.querySelector(".ans4");
-  answer4.innerHTML = Cards.beautyCard.choice4;
-}
+console.log(Cards);
