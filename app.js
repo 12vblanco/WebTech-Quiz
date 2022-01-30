0d88036b882bb90aa148294ad48c7f5f606d9515
Victor Blanco <victorblanco@ordersmadesimple.com>
2022-01-30 09:41:14 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const loveCard = {
+  answer: "Love",
+  choice1: "Love",
+  choice2: "Hate",
+  choice3: "Pain",
+  choice4: "Hope",
+  pattern: "background : #ff0000",
+  colour1: ["height : 90%", "background : #ff0000"],
+  colour2: ["height : 0%", "background : #ff0000"],
+  colour3: ["height : 0%", "background : #ff0000"],
+  colour4: ["height : 10%", "background : #ea6284"],
+};
