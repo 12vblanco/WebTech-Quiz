f7808cb2af3ce28cccfb1121a55fe29f168e4888
Victor Blanco <victorblanco@ordersmadesimple.com>
2022-01-31 20:04:25 +0000

    extra js removed

--- js/app.js
+++ js/app.js
@@ -13,31 +13,3 @@ const colour2 = document.querySelector(".colour2");
 const colour3 = document.querySelector(".colour3");
 const colour4 = document.querySelector(".colour4");
 const answer = document.querySelectorAll("#answer");
-
-let currentQuestion = {};
-let acceptingAnswers = true;
-let questionCounter = 0;
-let availableQuestions = [];
-
-const max_questions = 5;
-
-startGame = () => {
-  questionCounter = 0;
-  availableQuestions = [...cards];
-  getNewQuestion();
-};
-
-getNewQuestion = () => {
-  if (availableQuestions.length === 0 || questionCounter > max_questions)
-    return window.location.assign("/end.html");
-  questionCounter++;
-  counter.innerText = `${questionCounter} / ${max_questions}`;
-
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-  currentQuestion = availableQuestions[questionIndex];
-
-  answer.innerText = cards[0].answer;
-};
-colour1.setAttribute("style", "background : #646835");
-colour2.setAttribute("style", "background : #fff");
-colour4.setAttribute("style", "background : #6C8491");
