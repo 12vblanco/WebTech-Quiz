b26396f696f0a2a63ccb9f29cff336f625fa7a04
Victor Blanco <victorblanco@ordersmadesimple.com>
2022-03-18 10:34:15 +0000

    variables added

--- js/app.js
+++ js/app.js
@@ -1,15 +1,37 @@
-import * as cards from "./cards.js";
+// import { cards } from "./cards.js";
 
-console.log(cards);
+// console.log(cards.love);
+// console.log(cards.hate);
+// console.log(cards);
 
-const options = document.getElementById("options");
-const cardBtn = document.getElementById("card_btn");
-const counter = document.getElementById("counter");
-const btnQuiz = document.getElementById("btn_quiz");
-const btnTutorial = document.getElementById("btn_tutorial");
-const pattern = document.querySelector("#pattern");
-const colour1 = document.querySelector(".colour1");
-const colour2 = document.querySelector(".colour2");
-const colour3 = document.querySelector(".colour3");
-const colour4 = document.querySelector(".colour4");
-const answer = document.querySelectorAll("#answer");
+// //quiz container
+// const quizCont = document.getElementById("card");
+// // submit quiz
+// const cardBtn = document.getElementById("card_btn");
+// //quiz results
+// const results = document.getElementById("results");
+
+// const options = document.getElementById("options");
+// const counter = document.getElementById("counter");
+// const pattern = document.querySelector("#pattern");
+// // right answer and ID
+// const answers = document.querySelectorAll("#answer");
+
+// // home buttons
+// const btnQuiz = document.getElementById("btn_quiz");
+// const btnTutorial = document.getElementById("btn_tutorial");
+
+// // tutorial https://www.sitepoint.com/simple-javascript-quiz/
+// function createQuiz() {
+//   const output = [];
+
+//   quizQuestions.forEach((currentQuestion, cards.quizQuestions.answers) => {
+//     const answers = [];
+//     for(ans in currentQuestion.quizA)
+//   });
+// }
+// function Results() {}
+
+// createQuiz();
+
+// submitButton.addEventListener("click", Results);
--- js/cards.js
+++ js/cards.js
@@ -1,57 +1,64 @@
-export const Cards = [
+// export const love = {
+//   id: "Love",
+//   choice1: "Love",
+//   choice2: "Hate",
+//   choice3: "Pain",
+//   choice4: "Hope",
+//   pattern: "img/patterns/love.svg",
+// };
+// export const hate = {
+//   id: "Hate",
+//   choices: ["Love", "Hate", "Pain", "Hope"],
+//   pattern: "img/patterns/hate.svg",
+// };
+// export const heat = {
+//   id: "Heat",
+//   choice1: "Calm",
+//   choice2: "Heat",
+//   choice3: "Excitement",
+//   choice4: "Natural",
+//   pattern: "img/patterns/heat.svg",
+// };
+// export const beauty = {
+//   id: "Beauty",
+//   choice1: "The Lie",
+//   choice2: "Light",
+//   choice3: "Beauty",
+//   choice4: "Artificial",
+//   pattern: "img/patterns/beauty.svg",
+// };
+// export const power = {
+//   id: "Power",
+//   choice1: "Frivolous",
+//   choice2: "Light",
+//   choice3: "Original",
+//   choice4: "Power",
+//   pattern: "img/patterns/power.svg",
+// };
+
+// export * as cards from "./cards.js";
+
+const quizQuestions = [
   {
-    answer: "Love",
-    choice1: "Love",
-    choice2: "Hate",
-    choice3: "Pain",
-    choice4: "Hope",
-    colour1: ["height : 90%", "background : #ff0000"],
-    colour2: ["height : 0%", "background : #ff0000"],
-    colour3: ["height : 0%", "background : #ff0000"],
-    colour4: ["height : 10%", "background : #ea6284"],
+    id: "Love",
+    choices: ["Love", "Hate", "Pain", "Hope"],
+    pattern: "img/patterns/love.svg",
   },
   {
-    answer: "Hate",
-    choice1: "Love",
-    choice2: "Hate",
-    choice3: "Pain",
-    choice4: "Hope",
-    colour1: ["height : 55%", "background : #ff0000"],
-    colour2: ["height : 0%", "background : #ff0000"],
-    colour3: ["height : 35%", "background : #000"],
-    colour4: ["height : 10%", "background : #fff504"],
-  },
-  {
-    answer: "Heat",
+    id: "Heat",
     choice1: "Calm",
     choice2: "Heat",
     choice3: "Excitement",
     choice4: "Natural",
-    colour1: ["height : 55%", "background : #ff0000"],
-    colour2: ["height : 0%", "background : #ff0000"],
-    colour3: ["height : 35%", "background : #fff504"],
-    colour4: ["height : 10%", "background : #ffa504"],
+    pattern: "img/patterns/heat.svg",
   },
   {
-    answer: "Beauty",
+    id: "Beauty",
     choice1: "The Lie",
     choice2: "Light",
     choice3: "Beauty",
     choice4: "Artificial",
-    colour1: ["height : 25%", "background : #646835"],
-    colour2: ["height : 26%", "background : #fff"],
-    colour3: ["height : 29%", "background : #ff0000"],
-    colour4: ["height : 20%", "background : #6C8491"],
-  },
-  {
-    answer: "Power",
-    choice1: "Frivolous",
-    choice2: "Light",
-    choice3: "Original",
-    choice4: "Power",
-    colour1: ["height : 25%", "background : #646835"],
-    colour2: ["height : 26%", "background : #fff"],
-    colour3: ["height : 29%", "background : #ff0000"],
-    colour4: ["height : 20%", "background : #6C8491"],
+    pattern: "img/patterns/beauty.svg",
   },
 ];
+// to short out the questions in random order https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
