74bb623d45c427185e890c4e9413f9b4389bf5dc
Victor Blanco <victorblanco@ordersmadesimple.com>
2022-03-27 17:53:57 +0100

    update 270322

--- js/app.js
+++ js/app.js
@@ -1,37 +1,18 @@
-// import { cards } from "./cards.js";
+// LINKS
 
-// console.log(cards.love);
-// console.log(cards.hate);
-// console.log(cards);
+const card = document.getElementById("card2");
+const flipButton = document.querySelector(".side-two");
+const goToQuiz = document.querySelector(".start-quiz");
+const goToQuiz2 = document.querySelector(".tutorial-start-quiz");
 
-// //quiz container
-// const quizCont = document.getElementById("card");
-// // submit quiz
-// const cardBtn = document.getElementById("card_btn");
-// //quiz results
-// const results = document.getElementById("results");
+flipButton.onclick = function () {
+  if (card.className == "card2_back") card.className = "card2_front";
+  else card.className = "card2_back";
+};
 
-// const options = document.getElementById("options");
-// const counter = document.getElementById("counter");
-// const pattern = document.querySelector("#pattern");
-// // right answer and ID
-// const answers = document.querySelectorAll("#answer");
-
-// // home buttons
-// const btnQuiz = document.getElementById("btn_quiz");
-// const btnTutorial = document.getElementById("btn_tutorial");
-
-// // tutorial https://www.sitepoint.com/simple-javascript-quiz/
-// function createQuiz() {
-//   const output = [];
-
-//   quizQuestions.forEach((currentQuestion, cards.quizQuestions.answers) => {
-//     const answers = [];
-//     for(ans in currentQuestion.quizA)
-//   });
-// }
-// function Results() {}
-
-// createQuiz();
-
-// submitButton.addEventListener("click", Results);
+goToQuiz.onclick = function goToQuiz() {
+  location.href = "./quiz.html";
+};
+goToQuiz2.onclick = function goToQuiz() {
+  location.href = "./quiz.html";
+};
